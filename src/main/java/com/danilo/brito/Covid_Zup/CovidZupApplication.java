4d3b67d6e2d12e32b0acbd7d70f2415902a7d960
Danilo Brito <danilomelo.brito19@gmail.com>
2021-03-17 21:28:15 -0300

    Initial commit

---
+++ src/main/java/com/danilo/brito/Covid_Zup/CovidZupApplication.java
@@ -0,0 +1,13 @@
+package com.danilo.brito.Covid_Zup;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CovidZupApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CovidZupApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/danilo/brito/Covid_Zup/CovidZupApplicationTests.java
@@ -0,0 +1,13 @@
+package com.danilo.brito.Covid_Zup;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CovidZupApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
